d568d2f7421f412c9fcf49503fe7e0694650703e
Pedro Ferrari <108700770+pferrarip@users.noreply.github.com>
2024-03-18 19:37:29 -0300

    Add files via upload

---
+++ semáforo.cpp
@@ -0,0 +1,21 @@
+// C++ code
+//
+void setup()
+{
+    pinMode(0, OUTPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(0, LOW);
+  digitalWrite(1, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  delay(5000); // Wait for 5000 millisecond(s)
+  digitalWrite(2, LOW);
+}
